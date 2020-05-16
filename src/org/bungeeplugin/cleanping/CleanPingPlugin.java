ab270d0b09ac289d7b001bbbdbea4d7c0e1f31a1
oscar <oscar@oscar-PC>
2020-05-16 10:57:00 +0200

    First commit.

---
+++ src/org/bungeeplugin/cleanping/CleanPingPlugin.java
@@ -0,0 +1,27 @@
+package org.bungeeplugin.cleanping;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.ServerPing.Protocol;
+import net.md_5.bungee.api.event.ProxyPingEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
+
+public class CleanPingPlugin extends Plugin implements Listener {
+	
+	@Override
+	public void onEnable() {
+		ProxyServer.getInstance().getPluginManager().registerListener(this, this);
+	}
+	
+	@EventHandler
+	public void onPing(ProxyPingEvent e) {
+		ServerPing serverPing = e.getResponse();
+		Protocol protocol = serverPing.getVersion();
+		protocol.setName(ProxyServer.getInstance().getVersion().split(":")[2].split("-")[0]);
+		serverPing.setVersion(protocol);
+		e.setResponse(serverPing);
+	}
+	
+}
